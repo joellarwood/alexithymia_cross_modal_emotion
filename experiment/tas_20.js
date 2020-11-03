96830445ba7f2fd35cef8db34a4112a136c13012
Joel larwood <joel.larwood@uq.net.au>
2020-11-03 17:02:16 +1000

    create music block sans practice

---
+++ experiment/tas_20.js
@@ -0,0 +1,91 @@
+var tas_20 = [
+  {
+    "item": "I am often confused about what emotion I am feeling",
+    "tas_no": "tas_1"
+  },
+  {
+    "item": "It is difficult for me to find the right words for my feelings",
+    "tas_no": "tas_2"
+  },
+  {
+    "item": "I have physical sensations that even doctors don’t understand",
+    "tas_no": "tas_3"
+  },
+  {
+    "item": "I am able to describe my feelings easily",
+    "tas_no": "tas_4"
+  },
+  {
+    "item": "I prefer to analyse problems rather than just describe them",
+    "tas_no": "tas_5"
+  },
+  {
+    "item": "When I am upset, I don’t know if I am sad, frightened, or angry",
+    "tas_no": "tas_6"
+  },
+  {
+    "item": "I am often puzzled by sensations in my body",
+    "tas_no": "tas_7"
+  },
+  {
+    "item": "I prefer to just let things happen than to understand why they turned out that way",
+    "tas_no": "tas_8"
+  },
+  {
+    "item": "I have feelings that I can’t quite identify",
+    "tas_no": "tas_9"
+  },
+  {
+    "item": "Being in touch with emotions is essential",
+    "tas_no": "tas_10"
+  },
+  {
+    "item": "I find it hard to describe how I feel about people",
+    "tas_no": "tas_11"
+  },
+  {
+    "item": "People tell me to describe my feelings more",
+    "tas_no": "tas_12"
+  },
+  {
+    "item": "I don’t know what’s going on inside me",
+    "tas_no": "tas_13"
+  },
+  {
+    "item": "I often don’t know why I am angry",
+    "tas_no": "tas_14"
+  },
+  {
+    "item": "I prefer talking to people about their daily activities rather than their feelings",
+    "tas_no": "tas_15"
+  },
+  {
+    "item": "I prefer to watch ‘light’ entertainment shows rather than psychological dramas",
+    "tas_no": "tas_16"
+  },
+  {
+    "item": "It is difficult for me to reveal my innermost feelings, even to someone close",
+    "tas_no": "tas_17"
+  },
+  {
+    "item": "I can feel close to someone, even in moments of silence",
+    "tas_no": "tas_18"
+  },
+  {
+    "item": "I find examination of my feelings useful in solving personal problems",
+    "tas_no": "tas_19"
+  },
+  {
+    "item": "Looking for hidden meanings in movies or plays distracts from their enjoyment",
+    "tas_no": "tas_20"
+  }
+]
+
+
+var tas_scale = [
+  "Strongly Disagree", 
+  "Disagree", 
+  "Neutral", 
+  "Agree", 
+  "Strongly Agree"
+]
