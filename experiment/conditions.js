de96fff5799145113e37c21326c40ddee9cd5aed
Joel larwood <joel.larwood@uq.net.au>
2020-11-05 12:04:05 +1000

    put all trials into conditions.js

---
+++ experiment/conditions.js
@@ -0,0 +1,434 @@
+var conditions = [
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/21_Skank_Down.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/21_Skank_Down.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/21_Skank_Down.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/21_Skank_Down.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/21_Skank_Down.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/21_Skank_Down.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/26_Make_Light.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/26_Make_Light.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/26_Make_Light.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/26_Make_Light.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/26_Make_Light.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/26_Make_Light.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/1_Be_Happy.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/1_Be_Happy.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/1_Be_Happy.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/1_Be_Happy.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/1_Be_Happy.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/1_Be_Happy.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/5_Valerie.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/5_Valerie.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/5_Valerie.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/5_Valerie.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/5_Valerie.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "happy",
+    "cue": "audio/5_Valerie.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/2_Loves_Requiem.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/2_Loves_Requiem.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/2_Loves_Requiem.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/2_Loves_Requiem.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/2_Loves_Requiem.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/2_Loves_Requiem.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/8_Fire_And_Rain__(LP_Version).mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/8_Fire_And_Rain__(LP_Version).mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/8_Fire_And_Rain__(LP_Version).mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/8_Fire_And_Rain__(LP_Version).mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/8_Fire_And_Rain__(LP_Version).mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/8_Fire_And_Rain__(LP_Version).mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/3_Old_Blacktop.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/3_Old_Blacktop.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/3_Old_Blacktop.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/3_Old_Blacktop.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/3_Old_Blacktop.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/3_Old_Blacktop.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/12_Skinny_Love.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/5_Get_Lucky.mp3",
+    "target_word": "happy",
+    "target_face": "img/BF34HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/12_Skinny_Love.mp3",
+    "target_emotion": "happy",
+    "target_song": "audio/16_Restless.mp3",
+    "target_word": "happy",
+    "target_face": "img/BM08HAS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/12_Skinny_Love.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/7_Here_Today.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF34SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/12_Skinny_Love.mp3",
+    "target_emotion": "sad",
+    "target_song": "audio/2_Inside.mp3",
+    "target_word": "sad",
+    "target_face": "img/BF08SAS.jpg",
+    "condition": "congruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/12_Skinny_Love.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/13_Motown_Junk.mp3",
+    "target_word": "angry",
+    "target_face": "img/BF34ANS.jpg",
+    "condition": "incongruent"
+  },
+  {
+    "cue_emotion": "sad",
+    "cue": "audio/12_Skinny_Love.mp3",
+    "target_emotion": "angry",
+    "target_song": "audio/2_Incomplete.mp3",
+    "target_word": "angry",
+    "target_face": "img/BM08ANS.jpg",
+    "condition": "incongruent"
+  }
+]
